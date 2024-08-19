30af54ef5ede4fda15c6ae52339faf7b7d58da6c
Marcin Jędrzejak <mjedrzejak@icloud.com>
2024-08-19 11:56:27 +0200

    feat: Overview and first MeshGradient

--- MeshGradients/Introduction.swift
+++ MeshGradients/Introduction.swift
@@ -10,8 +10,27 @@ import SwiftUI
 struct Introduction: View {
     var body: some View {
         NavigationStack {
-            Text("MeshGradient")
-                .navigationTitle("Introduction")
+            MeshGradient(
+                width: 3,
+                height: 4,
+                points: [
+                    [0, 0], [0.5, 0.5], [1.0, 0],
+                    [0, 0.33], [0.5, 0], [1.0, 0.33],
+                    [0, 0.67], [0.25, 0.75], [1.0, 0.67],
+                    [0, 1.0], [0.25, 0.5], [1.0, 1.0]
+                ],
+                colors: [
+                    .red, .blue, .green,
+                    .teal, .orange, .yellow,
+                    .purple, .cyan, .indigo,
+                    .mint, .black, .white
+                ],
+                background: .green.opacity(0.2),
+                smoothsColors: false
+            )
+            .shadow(color: .gray, radius: 15)
+            .ignoresSafeArea(edges: .bottom)
+            .navigationTitle("Introduction")
         }
     }
 }
