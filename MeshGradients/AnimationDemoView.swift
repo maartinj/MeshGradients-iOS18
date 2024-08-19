63566b149785dfe990b894f6fd74d135fb56c450
Marcin Jędrzejak <mjedrzejak@icloud.com>
2024-08-19 12:10:45 +0200

    feat: Adding Animation

--- MeshGradients/AnimationDemoView.swift
+++ MeshGradients/AnimationDemoView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct AnimationDemoView: View {
+    
+    let timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+    @State private var colors: [Color] = AppData.randomColors()
+    
     var body: some View {
-        Text("MeshGradient")
-            .navigationTitle("Animation Demo")
+        MeshGradient(
+            width: 3,
+            height: 3,
+            points: AppData.points,
+            colors: colors
+        )
+        .ignoresSafeArea(edges: .bottom)
+        .onReceive(timer, perform: { _ in
+            withAnimation(.easeInOut(duration: 3)){
+                colors = AppData.randomColors()
+            }
+        })
+        .navigationTitle("Animation Demo")
     }
 }
 #Preview {
